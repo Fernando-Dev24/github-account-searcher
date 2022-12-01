639f697721e731c0dc82db497210d220272d048c
Fernando Ortiz <fernandodev.ortiz@gmail.com>
2022-12-01 13:48:18 -0600

    User changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+/* components */
+import { App } from './App';
+/* styles */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  <App />
+);
